d8b0ad22683d90bc700a934ffd5bb6590ff17014
dongyun <han10091009@naver.com>
2022-02-12 10:32:50 +0900

    test

--- src/elements/dongyun.js
+++ src/elements/dongyun.js
@@ -8,4 +8,6 @@ const dongyun = () => {
   );
 };
 
+const div = styled.div;
+
 export default dongyun;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 import React from "react";
+import Post from "../components/Post";
+
 import { useSelector } from "react-redux";
 
 const Home = () => {
