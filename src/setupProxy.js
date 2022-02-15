d761694076d4fd49c8b65bbaba1c50c6af60cebc
act99 <dorxm999@gmail.com>
2022-02-15 19:43:48 +0900

    please

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/user",
     createProxyMiddleware({
-      target: "http://3.36.65.28:8080",
+      target: "https://3.36.65.28:8080",
       changeOrigin: true,
     })
   );
